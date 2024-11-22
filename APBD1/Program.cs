f8272f43e3751e4b11d0253eb3813668cf217488
s16649 <s16649@pjwstk.edu.pl>
2024-11-22 12:54:46 +0100

    add CalculateAverage

--- APBD1/Program.cs
+++ APBD1/Program.cs
@@ -4,3 +4,24 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
+
+static double CalculateAverage(int[] numbers)
+{
+    if (numbers == null || numbers.Length == 0)
+    {
+        throw new ArgumentException("Tablica nie moze byc pusta i/lub byc nullem");
+    }
+
+    int sum = 0;
+
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        sum += numbers[i];
+    }
+
+    return (double)sum / numbers.Length;
+}
+
+int[] numbers = { 10, 20, 30, 40, 50 };
+double average = CalculateAverage(numbers);
+Console.WriteLine(average);
